1967bd80f795b7b0d03521860c96756bcc5699c2
Stefan2517 <stefiarko803@gmail.com>
2023-02-06 10:45:54 +0200

    second commit

--- aplicatie/principal/views.py
+++ aplicatie/principal/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 
+from articol.models import Category, Articol
+
 def index(request):
-	return render(request, 'principal/index.html')
+	articole = Articol.objects.filter(is_sold=False)[0:6]
+	Marci = Category.objects.all()
+
+	return render(request, 'principal/index.html',{
+		'Marci': Category,
+		'articole': articole,
+	})
 
 def contact(request):
 	return render(request, 'principal/contact.html')
